e27b137f23b059633f2cec274d0a0f25b7376d18
Job <jobsmith9@gmail.com>
2018-04-05 09:38:22 -0600

    updated db manager

--- knexfile.js
+++ knexfile.js
@@ -11,7 +11,6 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
-    DATABASE_URL: "postgres://vplzpmfcdrncaa:04564f0812735a95cdec740298ef0e7b355991ea8d8e89ae7149b643d3079e9c@ec2-54-243-130-33.compute-1.amazonaws.com:5432/dcr0bbojalt7n4"
 
 
   }
--- queries.js
+++ queries.js
@@ -8,10 +8,10 @@ module.exports = {
       return database("coffee").select().where("id", id).first()
     },
     create(coffee){
-      return database("coffee").insert(resolution).returning("*").then(record => record[0])
+      return database("coffee").insert(coffee).returning("*").then(record => record[0])
     },
     update(id, coffee){
-      return database("coffee").update(resolution).where("id", id).returning("*").then(record => record[0])
+      return database("coffee").update(coffee).where("id", id).returning("*").then(record => record[0])
     },
     delete(id){
       return database("coffee").delete().where("id", id)
